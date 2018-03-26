9201f0e12bcc0b9b65a711ab7a15469c8f378a67
robtot <rytovro@ixonos.local>
2018-03-26 15:16:45 +0300

    changed appspec to use ubuntu user and install pm2

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,7 +2,7 @@
 echo "Starting application-start script"
 whoami
 echo $PATH
-export PATH=/root/.nvm/versions/node/v6.11.5/bin:$PATH
+export PATH=/home/ubuntu/.nvm/versions/node/v6.11.5/bin:$PATH
 echo $PATH
 cd /var/api
 npm start
--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,15 +2,23 @@
 echo "Starting install-dependencies script"
 whoami
 echo $PATH
-
-export PATH=/root/.nvm/versions/node/v6.11.5/bin:$PATH
+export PATH=/home/ubuntu/.nvm/versions/node/v6.11.5/bin:$PATH
 echo $PATH
 
 if which node > /dev/null
     then
         echo "node is installed, skipping..."
     else
+        echo "installing node 6.11.5..."
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
         . ~/.nvm/nvm.sh
         nvm install 6.11.5
+    fi
+
+if which pm2 > /dev/null
+    then
+        echo "pm2 is installed, skipping..."
+    else
+        echo "installing pm2..."
+        npm install -g pm2
     fi
