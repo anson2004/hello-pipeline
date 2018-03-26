f9eb557bf4f680af44187c8cc8f3c8f54a7f4d1e
robtot <rytovro@ixonos.local>
2018-03-26 16:22:34 +0300

    first app change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function(request, response){
   response.writeHead(200, {'Content-type':'text/plan'});
-  response.write('Hello Node JS Server Response');
+  response.write('WAZAAP');
   response.end( );
 }).listen(7000);
