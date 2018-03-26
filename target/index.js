f4d8ec959d1665827c668d6efff825517c1ad52b
robtot <rytovro@ixonos.local>
2018-03-26 11:57:16 +0300

    buildspec artifact configuration change

---
+++ target/index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(request, response){
+  response.writeHead(200, {'Content-type':'text/plan'});
+  response.write('Hello Node JS Server Response');
+  response.end( );
+}).listen(7000);
