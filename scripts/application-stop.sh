2c33949c9cea4f4dda067ac725161f9621e9b4d3
Liang Yiqing <yiqing.liang@digitalistgroup.com>
2018-03-27 10:59:41 +0300

    add source 2

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -2,7 +2,7 @@
 echo "Starting application-stop script"
 whoami
 echo $PATH
-export PATH=/home/ubuntu/.nvm/versions/node/v6.11.5/bin:$PATH
+source ~/.bash_profile
 echo $PATH
 cd /var/api
 pm2 stop "hello-pipeline"
