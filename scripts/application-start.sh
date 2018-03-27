2aba666f65b0ed7a15cff4ed3c7fb7e78e013b3a
Liang Yiqing <yiqing.liang@digitalistgroup.com>
2018-03-27 13:56:15 +0300

    add pm2 delete

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -5,4 +5,4 @@ echo $PATH
 source ~/.bash_profile
 echo $PATH
 cd /var/api
-pm2 restart index.js --name "hello-pipeline"
+pm2 start index.js --name "hello-pipeline"
--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -5,7 +5,4 @@ echo $PATH
 source ~/.bash_profile
 echo $PATH
 cd /var/api
-pm2 stop "hello-pipeline"
-if [ ! $? -eq 0 ]; then
-    echo "pm2 stop failing""
-fi
+pm2 delete "hello-pipeline"
